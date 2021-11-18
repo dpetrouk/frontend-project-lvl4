dad8c517f38e1700ce49ca6549b01356058fcb0d
Dmitry Petrouk <dpetrouk@gmail.com>
2021-11-18 14:12:47 +0300

    move components out

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  console.log('Home');
+
+  return (
+    <h2>
+      Home
+    </h2>
+  );
+};
+
+export default Home;
---
+++ src/components/NoMatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoMatch = () => {
+  console.log('Not found');
+  return (
+    <p>
+      Page not found
+    </p>
+  );
+};
+
+export default NoMatch;
