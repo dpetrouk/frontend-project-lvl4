41869d3892dd8e3dcdf6b30742b71fb8e95b6b98
Dmitry Petrouk <dpetrouk@gmail.com>
2021-12-29 19:38:32 +0300

    add socket connection

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
 
 import useAuth from '../hooks/index.jsx';
 import { addChannel, channelsSelectors } from '../slices/channelsSlice.js';
@@ -10,18 +11,47 @@ const renderChannels = (channels) => channels.map(({ id, name }) => (
   <li key={id}>{name}</li>
 ));
 
-const renderMessages = (messages) => messages.map(({ id, name }) => (
-  <div key={id}>{name}</div>
+const renderMessages = (messages) => messages.map(({ username, id, body }) => (
+  <div key={id}>
+    <strong>{username}</strong>
+    <span>: </span>
+    {body}
+  </div>
 ));
 
 const Home = () => {
-  console.log('Home');
+  // console.log('Home');
   const dispatch = useDispatch();
-  const { token } = useAuth();
-  useEffect(() => dispatch(fetchData(token)), []);
+  const { token, username } = useAuth();
+  const [message, setMessage] = useState('');
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    const dispatchFetchData = () => dispatch(fetchData(token));
+    dispatchFetchData();
+    socketRef.current = io('ws://localhost:5000'); // ?
+    socketRef.current.on('connect', () => {
+      console.log('socket: connect');
+    });
+    socketRef.current.on('newMessage', () => {
+      dispatchFetchData();
+      setMessage('');
+    });
+    socketRef.current.on('newChannel', dispatchFetchData);
+    socketRef.current.on('removeChannel', dispatchFetchData);
+    socketRef.current.on('renameChannel', dispatchFetchData);
+  }, []);
+
   const channels = useSelector(channelsSelectors.selectAll);
   const messages = useSelector(messagesSelectors.selectAll);
-  console.log({ channels, messages });
+  // console.log({ username, channels, messages });
+
+  const handleMessageInput = (e) => setMessage(e.target.value);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socketRef.current.emit('newMessage', { username, body: message });
+  };
 
   return (
     <div className="container">
@@ -37,8 +67,11 @@ const Home = () => {
           <div></div>
           <div>{renderMessages(messages)}</div>
           <div>
-            <form>
-              <input />
+            <form onSubmit={sendMessage}>
+              <input
+                onChange={handleMessageInput}
+                value={message}
+              />
               <button>Send</button>
             </form>
           </div>
