6b5c01ab0dfaffae4ebc5a0a1335c127fc34070d
Dmitry Petrouk <dpetrouk@gmail.com>
2022-02-26 14:53:04 +0300

    rewrite state selector

--- src/components/modals/getValidationSchema.js
+++ src/components/modals/getValidationSchema.js
@@ -1,9 +1,8 @@
 import * as yup from 'yup';
 import { useSelector } from 'react-redux';
-import { channelsSelectors } from '../../slices/channelsSlice.js';
 
 const getChannelNames = () => {
-  const channels = useSelector(channelsSelectors.selectAll);
+  const channels = useSelector((state) => state.channelsInfo.channels);
   const channelsNames = channels.map(({ name }) => name);
   return channelsNames;
 };
