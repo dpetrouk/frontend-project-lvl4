d714683d5a938da80dd13915ecd0c490467f729f
Dmitry Petrouk <dpetrouk@gmail.com>
2022-02-26 14:18:02 +0300

    add modal state management

---
+++ src/slices/modalSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isOpened: false,
+  type: null,
+  extra: null,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state, action) => ({
+      isOpened: true,
+      type: action.payload.type,
+      extra: action.payload.extra,
+    }),
+    closeModal: (state) => {
+      state.isOpened = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
